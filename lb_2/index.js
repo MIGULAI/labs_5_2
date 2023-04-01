43a7bfa09cda985bc885c06f0f67eb89f8a0ba9a
Migulai <vitukivan05@gmail.com>
2023-04-01 18:03:44 +0300

    lb_2

---
+++ lb_2/index.js
@@ -0,0 +1,170 @@
+
+/**
+ * варіант №3 
+ * Кросовер: одноточковий
+ * Вибір батьків: Колесо релетки
+ * Функція пристосованості: adjustment()
+ */
+const groupsNumb = 6    // кількість груп
+const groups = []
+const migrNumb = 2
+const num = 2
+let minA = 0.5
+let maxA = 1.1
+let dom = {}
+let A = 0.5
+const createX = () => {
+    return minA + (maxA - minA) * Math.random()
+}
+let minB = 1
+let maxB = 4.6
+const createY = () => {
+    return minB + (maxB - minB) * Math.random()
+}
+const adjustment = (x, y) => {
+    return ((-2 * Math.pow(y, 3) + 6 * Math.pow(y, 2) + 6 * y) * Math.sin(Math.log(x) * Math.pow(Math.E, y)))
+}
+
+const generateСhromosome = () => {
+    let chromosome = {
+        x: createX(),
+        y: createY(),
+    }
+    chromosome.fitness = adjustment(chromosome.x, chromosome.y)
+    return chromosome
+}
+
+const dec2bin = (dec) => {
+    return dec.toString(2)
+}
+
+function compare(a, b) {
+    if (a.fitness < b.fitness) {
+        return 1;
+    }
+    if (a.fitness > b.fitness) {
+        return -1;
+    }
+    return 0;
+}
+
+
+
+const changeValues = () => {
+    let smthMinA = dom.x - A * (maxA - minA) / 2
+    let smthMaxA = dom.x - A * (maxA - minA) / 2
+    let smthMinB = dom.y - A * (maxB - minB) / 2
+    let smthMaxB = dom.y - A * (maxB - minB) / 2
+    minA = smthMinA
+    maxA = smthMaxA
+    minB = smthMinB
+    maxA = smthMaxB
+}
+const changeValuesParalel = () => {
+    let smthMinA = minA
+    let smthMaxA = maxA
+    let smthMinB = minB
+    let smthMaxB = maxB
+    let domMinA = dom.x-smthMinA
+    let domMaxA = smthMaxA - dom.x
+    if(domMinA < domMaxA){
+        smthMinA = dom.x
+    }else{
+        smthMaxA = dom.x
+    }
+    let domMinB = dom.y-smthMinB
+    let domMaxB = smthMaxB - dom.y
+    if(domMinB < domMaxA){
+        smthMinB = dom.y
+    }else{
+        smthMaxB = dom.y
+    }
+    minA = smthMinA
+    maxA = smthMaxA
+    minB = smthMinB
+    maxA = smthMaxB
+}
+
+const migrate = (groups) => {
+    let migrationArray = []
+    for (let i = 0; i < groups.length; i++) {
+        let mArray = []
+        for (let j = 0; j < migrNumb; j++) {
+            const index = Math.floor(Math.random() * groups[i].length)
+            mArray.push(groups[i][index])
+            groups[i].splice(index, 1)
+        }
+        migrationArray.push(mArray)
+    }
+    for (let i = 0; i < groups.length; i++) {
+        const index = Math.floor(Math.random() * migrationArray.length)
+        for (let j = 0; j < migrationArray[index].length; j++) {
+            groups[i].push(migrationArray[index][j])
+        }
+        migrationArray.splice(index, 1)
+    }
+    //return groups
+}
+
+let liders = [] // лідери
+let liderBoard = []
+for (let iteration = 0; iteration < 10; iteration++) {
+
+
+    for (let i = 0; i < groupsNumb; i++) {
+        //groups
+
+        let iters = 5
+        const bits = 8  // бітність хромосоми
+        const numberChroms = 10 // кількість хромосом
+        // хромосоми
+        let t = 0 // ітерація
+        let pSum = 0
+        let n = Math.ceil(numberChroms / 10)
+
+        for (let index = 0; index < iters; index++) {
+            let P = []
+            for (let i = 0; i < numberChroms; i++) {
+                let chrom = generateСhromosome()
+                P.push(chrom)
+                pSum += chrom.fitness
+            }
+            // dom = arr[0]
+            groups[i] = P
+
+            //console.log(index);
+            //let best = arr.splice(0, n)
+            // liderBoard.push({ ...dom, iter: index })
+            // changeValues()
+        }
+
+
+        // liders.push(liderBoard[0]);
+
+
+
+    }
+    migrate(groups)
+    for (let i = 0; i < groups.length; i++) {
+        let arr = groups[i].sort(compare)
+        let smallDom = arr[0]
+        //dom = arr[0]
+       // let best = arr.splice(0, 1)
+        //liderBoard.sort(compare)
+        liderBoard.push({
+            ...smallDom
+        })
+    }
+    liderBoard.sort(compare)
+    dom = liderBoard[0]
+    liders.push(liderBoard[0])
+    changeValuesParalel()
+
+}
+liders.sort(compare)
+console.log(liders[0]);
+
+//let sortedLiders = liders.sort(compare)
+
+//console.log("Максимальний лідер :  " + "\nx:\t" + sortedLiders[0].x + '\ny:\t' + sortedLiders[0].y.toString() + "\nfitness:\t" + sortedLiders[0].fitness);
+//console.log("Мінімальний лідер : " + "\nx:\t" + sortedLiders[sortedLiders.length - 1].x + '\ny:\t' + sortedLiders[sortedLiders.length - 1].y + "\nfitness:\t" + sortedLiders[sortedLiders.length - 1].fitness);
